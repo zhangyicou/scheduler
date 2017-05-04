7deb3a24c1d93295c1af917800ae049756c7e51a
zhangyc01 <zhangyc01@shengjing360.com>
2017-05-04 15:03:13 +0800

    commit

--- src/main/java/com/shengjing/ibd/scheduler/quartz/SyncJobFactory.java
+++ src/main/java/com/shengjing/ibd/scheduler/quartz/SyncJobFactory.java
@@ -17,17 +17,16 @@ import org.springframework.web.client.RestTemplate;
 @PersistJobDataAfterExecution
 @DisallowConcurrentExecution
 public class SyncJobFactory extends QuartzJobBean {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    /* 日志对象 */
-    private static final Logger LOG = LoggerFactory.getLogger(SyncJobFactory.class);
     private RestTemplate restTemplate = new RestTemplate();
 
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        LOG.info("SyncJobFactory execute");
+        logger.info("SyncJobFactory execute");
         JobDataMap mergedJobDataMap = context.getMergedJobDataMap();
         ScheduleJob scheduleJob = (ScheduleJob) mergedJobDataMap.get(ScheduleJobVo.JOB_PARAM_KEY);
-        System.out.println("jobName:" + scheduleJob.getJobName() + "  " + scheduleJob);
+        logger.info("jobName:" + scheduleJob.getJobName() + "  " + scheduleJob);
         Object result = restTemplate.getForObject(scheduleJob.getUrl(), Object.class);
-        LOG.info("result:"+result.toString());
+        logger.info("result:"+result.toString());
     }
 }
