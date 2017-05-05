fbf3597024323a40d7fa4a90de8488a0ab0b5445
zhangyc01 <zhangyc01@shengjing360.com>
2017-05-05 15:56:19 +0800

    commit

--- src/main/java/com/shengjing/ibd/scheduler/config/AppConfig.java
+++ src/main/java/com/shengjing/ibd/scheduler/config/AppConfig.java
@@ -1,20 +1,28 @@
 package com.shengjing.ibd.scheduler.config;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.velocity.VelocityConfig;
+import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
+import org.springframework.web.servlet.view.velocity.VelocityToolboxView;
+import org.springframework.web.servlet.view.velocity.VelocityViewResolver;
 
 import javax.inject.Inject;
 
 @Configuration
-@Import(DataSourceConfig.class)
 public class AppConfig {
     private static Logger log = Logger.getLogger(AppConfig.class);
 
@@ -35,4 +43,27 @@ public class AppConfig {
         headers.add("Accept", MediaType.APPLICATION_JSON_UTF8_VALUE);
         return headers;
     }
+
+//    @Autowired
+//    private final ResourceLoader resourceLoader = new DefaultResourceLoader();
+//
+//    @Bean
+//    public VelocityConfig velocityConfig() {
+//        VelocityConfigurer cfg = new VelocityConfigurer();
+//        cfg.setResourceLoader(resourceLoader);
+//        cfg.setResourceLoaderPath("/templates/");
+//        return cfg;
+//    }
+//
+//    @Bean
+//    public ViewResolver viewResolver() {
+//        VelocityViewResolver resolver = new VelocityViewResolver();
+//        resolver.setViewClass(VelocityToolboxView.class);
+//        //resolver.setToolboxConfigLocation("classpath:/WEB-INF/toolbox.xml");
+//        resolver.setPrefix("/");
+//        resolver.setSuffix(".vm");
+//        resolver.setCache(false);
+//        resolver.setOrder(Ordered.LOWEST_PRECEDENCE - 20);
+//        return resolver;
+//    }
 }
--- src/main/java/com/shengjing/ibd/scheduler/controller/ScheduleJobController.java
+++ src/main/java/com/shengjing/ibd/scheduler/controller/ScheduleJobController.java
@@ -6,8 +6,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
